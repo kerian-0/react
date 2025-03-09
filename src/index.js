9c8ba41c8140f518d111542f5a1bc66b816fb790
kerian <james342506@gmail.com>
2025-03-09 14:55:11 +0630

    second

--- src/index.js
+++ src/index.js
@@ -2,24 +2,34 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
+const cardOne={
+  image:"https://th.bing.com/th/id/OIP.qYjxUSvCp5Hy5iJxs4crOwAAAA?w=324&h=324&rs=1&pid=ImgDetMain",
+title:"card One title",
+description:"card one description"
+}
+const cardTwo={
+  image:"https://th.bing.com/th/id/OIP.qYjxUSvCp5Hy5iJxs4crOwAAAA?w=324&h=324&rs=1&pid=ImgDetMain",
+title:"card Two title",
+description:"card Two description"
+}
+
 function CardList() {
   return (
     <main className="card-list">
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
+      <Card image={cardOne.image} title={cardOne.title} description={cardOne.description}/>
+      <Card image={cardTwo.image} title={cardTwo.title} description={cardTwo.description}/>
+      
 
     </main>
   );
 }
-const title="Card Title";
-function Card(){
+function Card(props){
+  console.log(props);
   return (
     <div className="card">
-      <img src="https://th.bing.com/th/id/OIP.qYjxUSvCp5Hy5iJxs4crOwAAAA?w=324&h=324&rs=1&pid=ImgDetMain" alt="cart-image"/>
-      <h1>{title}</h1>
-      <Description/>
+      <img src={props.image} alt="cart-image"/>
+      <h1>{props.title}</h1>
+      <p>{props.description}</p>
     </div>
   )
 
@@ -31,9 +41,9 @@ return <p style={{marginTop:"10px"}}>Loremaloremlorem</p>
 
 ReactDOM.createRoot(document.getElementById("root")).render(<CardList />);
 
-//return single element
-//div/section/article/Fragment/use
-//use camelCase
-//use className instead of class
-//formatting
-//use close
+// return single element
+// div/section/article/Fragment/use
+// use camelCase
+// use className instead of class
+// formatting
+// use close
---
+++ src/index1.js
@@ -0,0 +1,32 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import "./index.css";
+
+// function CardList() {
+//   return (
+//     <main className="card-list">
+//       <Card/>
+//       <Card/>
+//       <Card/>
+//       <Card/>
+
+//     </main>
+//   );
+// }
+// const title="Card Title";
+// function Card(){
+//   return (
+//     <div className="card">
+//       <img src="https://th.bing.com/th/id/OIP.qYjxUSvCp5Hy5iJxs4crOwAAAA?w=324&h=324&rs=1&pid=ImgDetMain" alt="cart-image"/>
+//       <h1>{title}</h1>
+//       <Description/>
+//     </div>
+//   )
+
+// }
+
+// function Description(){
+// return <p style={{marginTop:"10px"}}>Loremaloremlorem</p>
+// }
+
+// ReactDOM.createRoot(document.getElementById("root")).render(<CardList />);
